83f1b2491e43166b5ab27fce951406c6f5d637cd
yan <leoyanpc@gmail.com>
2022-01-22 16:08:54 +0800

    first commit

---
+++ src/test/java/com/cainiao/jjrest/JjrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cainiao.jjrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjrestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
